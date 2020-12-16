862f905c5ba07e0da4b9612bdf020f53673d7973
Oscar López <oscar.lopez@alibrate.com>
2020-12-16 11:35:36 -0300

    health page

---
+++ next.config.js
@@ -0,0 +1,5 @@
+{
+    env: {
+        API: 'internal-alibrate-frankfurt-priv-alb-1373655442.eu-central-1.elb.amazonaws.com'
+    }
+}
---
+++ pages/health.js
@@ -0,0 +1,5 @@
+export default function Health() {
+  return (
+    <h1>OK</h1>
+  )
+}
